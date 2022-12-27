ac77efd68d4f2cc32fe58345832727acd5a965d3
Stephen Martinez <104386666+SdMartinez13@users.noreply.github.com>
2022-12-26 18:49:11 -0800

    quiz data

--- pages/categories/[catName].js
+++ pages/categories/[catName].js
@@ -1,9 +1,61 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
+import prisma from '../../lib/prisma';
 
-const CatName = () => {
+const CatName = ({ q }) => {
     const router = useRouter();
     console.log(router, 'ROUTER');
-    return <div>I am HERE!!!</div>;
+    console.log(router.query.catName)
+
+    console.log(q, 'qqqqqqqq');
+    return (
+        <div className="bg-red-100 p-5">
+
+            {q.map(item => {
+                const b = '';
+                return (
+                    <Link href={`/q/${item.quiz_id}`} key={item.quiz_id} className="bg-blue-100 p-4 m-4 text-center inline-block">
+                        <p>{item.title}</p>
+                        <p>({item.questions?.length} questions)</p>
+                    </Link>
+                );
+            })}
+        </div>
+    );
 };
 
 export default CatName;
+
+export const getServerSideProps = async (context) => {
+    // const categories = await prisma.categories.findMany();
+    const name = context.params.catName;
+    // get the router params
+    // lowercase the url
+    // find the categbory name in lowercase (prisma)
+
+    const getQuizzes = await prisma.quizzes.findMany({
+        where: {
+            categories: {
+                some: {
+                    category: {
+                        name: {
+                            equals: name,
+                            mode: 'insensitive',
+                        },
+                    },
+                },
+            },
+        },
+        include: {
+            questions: {},
+        },
+    });
+
+    return {
+        props: { q: JSON.parse(JSON.stringify(getQuizzes)) },
+    };
+};
+
+// CatName.propTypes = {
+//     categories: PropTypes.array,
+// };
--- prisma/quizSeedData.js
+++ prisma/quizSeedData.js
@@ -13,64 +13,134 @@ const CATEGORIES = {
 const quizzes = [
     // NEW QUIZ EXAMPLE
     {
-        title: 'My first quiz',
-        description: 'Some fake data form a description', // OPTIONAL
+        title: 'Javascript 1',
+        description: 'Vanilla Javascript', // OPTIONAL
         is_active: true, // OPTIONAL
-        categories: [CATEGORIES.MOVIES, CATEGORIES.JAVASCRIPT],
+        categories: [CATEGORIES.JAVASCRIPT],
         questions: [
             {
-                question: 'What color is grass?',
+                question: 'Javascript is an _______ language?',
                 // orderChoices: true, // used to randomize choices or not
                 choices: [
-                    { correct: false, choice: 'red' },
-                    { correct: true, choice: 'green' },
-                    { correct: false, choice: 'green' },
-                    { correct: false, choice: 'purple' },
+                    { correct: false, choice: 'Object-Based' },
+                    { correct: true, choice: 'Object-Oriented' },
+                    { correct: false, choice: 'Procedural' },
+                    { correct: false, choice: 'None of the above' },
                 ],
                 is_active: true,
-                score: 5, // OPTIONAL
+                score: 1, // OPTIONAL
             },
             {
-                question: 'What color are school busses?',
+                question: 'Inside which HTML element do we put the JavaScript?',
+                // orderChoices: true, // used to randomize choices or not
                 choices: [
-                    { correct: false, choice: 'red' },
-                    { correct: false, choice: 'green' },
-                    { correct: true, choice: 'yellow' },
-                    { correct: false, choice: 'purple' },
+                    { correct: false, choice: '<scripting>' },
+                    { correct: true, choice: '<script>' },
+                    { correct: false, choice: '<javascript>' },
+                    { correct: false, choice: 'js' },
                 ],
                 is_active: true,
-                score: 8, // OPTIONAL
+                score: 1, // OPTIONAL
+            },
+            {
+                question: 'Which operator returns true if the two compared values are not equal?',
+                // orderChoices: true, // used to randomize choices or not
+                choices: [
+                    { correct: false, choice: '==!' },
+                    { correct: true, choice: '!==' },
+                    { correct: false, choice: '~' },
+                    { correct: false, choice: '!=' },
+                ],
+                is_active: true,
+                score: 1, // OPTIONAL
+            },
+            {
+                question: 'Which statement creates a new object using the Person constructor?',
+                choices: [
+                    { correct: false, choice: 'var student = Person()' },
+                    { correct: false, choice: 'var student = construct Person;' },
+                    { correct: true, choice: 'var student = new Person()' },
+                    { correct: false, choice: 'var student = construct Person();' },
+                ],
+                is_active: true,
+                score: 1, // OPTIONAL
+            },
+            {
+                question: 'Which of the following keywords is used to define a variable in Javascript?',
+                // orderChoices: true, // used to randomize choices or not
+                choices: [
+                    { correct: false, choice: 'function' },
+                    { correct: true, choice: 'const' },
+                    { correct: false, choice: 'if' },
+                    { correct: false, choice: 'else' },
+                ],
+                is_active: true,
+                score: 1, // OPTIONAL
             },
         ],
     },
     // END NEW QUIZ EXAMPLE
 
     {
-        title: 'Anoteher quizzzzz = CHANGE MEEEE',
-        description: 'Some fake data form a description', // OPTIONAL
+        title: 'Javascript 2',
+        description: 'Vanilla Javascript', // OPTIONAL
         is_active: true, // OPTIONAL
+        categories: [CATEGORIES.JAVASCRIPT],
         questions: [
             {
-                question: 'What color is grass?',
+                question: 'How can a datatype be declared to be a constant type?',
                 choices: [
-                    { correct: false, choice: 'red' },
-                    { correct: true, choice: 'green' },
-                    { correct: false, choice: 'green' },
-                    { correct: false, choice: 'purple' },
+                    { correct: false, choice: 'var' },
+                    { correct: true, choice: 'const' },
+                    { correct: false, choice: 'let' },
+                    { correct: false, choice: 'constant' },
                 ],
                 is_active: true,
-                score: 5, // OPTIONAL
+                score: 1, // OPTIONAL
             },
             {
-                question: 'What color are school busses?',
+                question: 'When an operators value is NULL, the typeof returned by the unary operator is:',
                 choices: [
-                    { correct: false, choice: 'red' },
-                    { correct: false, choice: 'green' },
-                    { correct: true, choice: 'yellow' },
-                    { correct: false, choice: 'purple' },
+                    { correct: false, choice: 'Boolean' },
+                    { correct: false, choice: 'Undefined' },
+                    { correct: true, choice: 'Object' },
+                    { correct: false, choice: 'Integer' },
                 ],
                 is_active: true,
-                score: 8, // OPTIONAL
+                score: 1, // OPTIONAL
+            },
+            {
+                question: 'What does the Javascript “debugger” statement do?',
+                choices: [
+                    { correct: false, choice: 'It will debug all the errors in the program at runtime' },
+                    { correct: false, choice: 'It will debug errors in the current statement if any' },
+                    { correct: true, choice: 'It acts as a breakpoint in a program' },
+                    { correct: false, choice: 'It will do nothing' },
+                ],
+                is_active: true,
+                score: 1, // OPTIONAL
+            },
+            {
+                question: 'Which function is used to serialize an object into a JSON string in Javascript?',
+                choices: [
+                    { correct: false, choice: 'parse()' },
+                    { correct: false, choice: 'convert()' },
+                    { correct: true, choice: 'stringify()' },
+                    { correct: false, choice: 'It will do nothing' },
+                ],
+                is_active: true,
+                score: 1, // OPTIONAL
+            },
+            {
+                question: 'Which of the following is not a Javascript framework?',
+                choices: [
+                    { correct: true, choice: 'cassandra' },
+                    { correct: false, choice: 'vue' },
+                    { correct: false, choice: 'react' },
+                    { correct: false, choice: 'node' },
+                ],
+                is_active: true,
+                score: 1, // OPTIONAL
             },
         ],
     },
